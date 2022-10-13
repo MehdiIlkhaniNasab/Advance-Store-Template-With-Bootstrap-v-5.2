b5426229714ed6542b332813056498869d8248a8
Mehdi Ilkhani <you@example.commehdiilkhaninasab7@gmail.com>
2022-10-13 22:17:49 +0330

    update single product page.

--- js/single-product.js
+++ js/single-product.js
@@ -1,11 +1,13 @@
 import { searchBtn } from './app.js'
 
 let btnsTab = document.querySelectorAll('.btns button');
+let productImgs = document.querySelectorAll('.product-colors div img');
 let targetTabId = null;
 let targetTab = null;
 
 
 
+// Active The Target Tab 
 btnsTab.forEach((btn) => {
     btn.addEventListener('click', (event) => {
         disableBtnAndTabPev();
@@ -16,12 +18,26 @@ btnsTab.forEach((btn) => {
     })
 })
 
+// Show The Target Images Products
+productImgs.forEach((img) => {
+    img.addEventListener('click', (event) => {
+        showTargetImgProduct(event)
+    })
+})
+
+
+const showTargetImgProduct = (event) => {
+    let mainImgProduct = document.querySelector('.prodcut-main-img img');
+    let srcTargetImg = event.target.src;
+    mainImgProduct.src = srcTargetImg;
+}
+
 
 
 // Remove Class Form Prev Btn And Tab
-function disableBtnAndTabPev() {
+const disableBtnAndTabPev = () => {
     let beforeBtnActive = document.querySelector('.active-btn');
     beforeBtnActive.classList.remove('active-btn');
     let beforetABActive = document.querySelector('.active-tab');
-    beforetABActive.classList.remove('active-tab'); 
+    beforetABActive.classList.remove('active-tab');
 }
